bf0be44090e76b9b651946fceb0d91763fa319b5
itresumesha <48509542+itresumesha@users.noreply.github.com>
2019-03-13 10:57:23 +0300

    Add files via upload

---
+++ precache-manifest.b84ee9919316fa78282228c06fd95ff6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "1aa56db6593bf3b9a2f3",
+    "url": "/static/js/main.921b6641.chunk.js"
+  },
+  {
+    "revision": "3791395ae6897930f09c",
+    "url": "/static/js/2.b19c2b74.chunk.js"
+  },
+  {
+    "revision": "1aa56db6593bf3b9a2f3",
+    "url": "/static/css/main.7022fb64.chunk.css"
+  },
+  {
+    "revision": "6e220d95e6b2deb88e64f3bc4042e121",
+    "url": "/index.html"
+  }
+];
